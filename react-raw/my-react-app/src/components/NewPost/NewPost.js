17f9a62ca37b794934858e896ac69dff640cb96b
guiziis <guigamoda@hotmail.com>
2025-04-05 20:50:55 -0300

    add eventListener

--- react-raw/my-react-app/src/components/NewPost/NewPost.js
+++ react-raw/my-react-app/src/components/NewPost/NewPost.js
@@ -1,11 +1,16 @@
 import classes from './NewPost.module.css';
 
 function NewPost() {
+
+  function changeBodyHandler(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p className={classes.inputContainer}>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p className={classes.inputContainer}>
         <label htmlFor="name">Your name</label>
