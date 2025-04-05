12f83880b9cd44185e49374f0371f5bacb290568
guiziis <guigamoda@hotmail.com>
2025-04-05 20:44:48 -0300

    add newPost card

--- react-raw/my-react-app/src/components/ListPosts/ListPost.js
+++ react-raw/my-react-app/src/components/ListPosts/ListPost.js
@@ -1,11 +1,15 @@
+import NewPost from "../NewPost/NewPost";
 import { PostComponent } from "../Post/Post";
 import classes from './ListPost.module.css';
 
 export function ListPostComponent() {
   return (
-    <ul className={classes.posts}>
-      <PostComponent author='teste' text='teste 2' />
-      <PostComponent author='let' text='const' />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <PostComponent author='teste' text='teste 2' />
+        <PostComponent author='let' text='const' />
+      </ul>
+    </>
   )
 }
