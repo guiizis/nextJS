74e88b292933351cc967f0da219d1d779c36a132
guiziis <guigamoda@hotmail.com>
2025-04-05 21:25:30 -0300

    lifting up a state

--- react-raw/my-react-app/src/components/ListPosts/ListPost.js
+++ react-raw/my-react-app/src/components/ListPosts/ListPost.js
@@ -1,14 +1,29 @@
 import NewPost from "../NewPost/NewPost";
 import { PostComponent } from "../Post/Post";
 import classes from './ListPost.module.css';
+import { useState } from "react";
 
 export function ListPostComponent() {
+  const [enteredText, setEnteredText] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredText(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <PostComponent author='teste' text='teste 2' />
-        <PostComponent author='let' text='const' />
+        <PostComponent author={enteredAuthor} text={enteredText} />
+        <PostComponent author="test" text="test 2" />
       </ul>
     </>
   )
--- react-raw/my-react-app/src/components/NewPost/NewPost.js
+++ react-raw/my-react-app/src/components/NewPost/NewPost.js
@@ -1,20 +1,15 @@
 import classes from './NewPost.module.css';
 
-function NewPost() {
-
-  function changeBodyHandler(event) {
-    console.log(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p className={classes.inputContainer}>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
       <p className={classes.inputContainer}>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" onChange={props.onAuthorChange} required />
       </p>
     </form>
   );
