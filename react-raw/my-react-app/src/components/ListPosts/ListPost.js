fdd6da946ac559a70f23166e5d896880236f0fa0
guiziis <guigamoda@hotmail.com>
2025-04-07 09:27:54 -0300

    add friendly msg

--- react-raw/my-react-app/src/components/ListPosts/ListPost.js
+++ react-raw/my-react-app/src/components/ListPosts/ListPost.js
@@ -27,10 +27,16 @@ export function ListPostComponent() {
         />
       </Modal>
       <ul className={classes.posts}>
-        {posts && posts.map(post => (
+        {posts.length > 0 && posts.map(post => (
           <PostComponent key={post.id} author={post.author} text={post.body} />
         ))}
       </ul>
+      {posts.length === 0 && (
+        <div style={{textAlign: 'center', color:'white'}}>
+          <h2>No posts found!</h2>
+          <h3>Make new Posts =)</h3>
+        </div>
+      )}
     </>
   )
 }
