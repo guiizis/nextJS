8a589d5932e202240b2c9eddb430b43b3bf5e24a
guiziis <guigamoda@hotmail.com>
2025-04-07 08:52:54 -0300

    add dinamic posts

--- react-raw/my-react-app/src/components/ListPosts/ListPost.js
+++ react-raw/my-react-app/src/components/ListPosts/ListPost.js
@@ -3,36 +3,23 @@ import { Modal } from "../ModalComponet/modal";
 import NewPost from "../NewPost/NewPost";
 import { PostComponent } from "../Post/Post";
 import classes from './ListPost.module.css';
-import { useState } from "react";
 
 export function ListPostComponent() {
-  const [enteredText, setEnteredText] = useState('test 1');
-  const [enteredAuthor, setEnteredAuthor] = useState('test 2');
+
   const {setSharedOpenModalValue} = useOpenModalSharedState()
 
-  function changeBodyHandler(event) {
-    setEnteredText(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   function closeModalHandler() {
-    setSharedOpenModalValue(false);
+    setSharedOpenModalValue(false)
   }
 
   return (
     <>
       <Modal>
         <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
           onCancel={closeModalHandler}
         />
       </Modal>
       <ul className={classes.posts}>
-        <PostComponent author={enteredAuthor} text={enteredText} />
         <PostComponent author="test" text="test 2" />
       </ul>
     </>
--- react-raw/my-react-app/src/components/NewPost/NewPost.js
+++ react-raw/my-react-app/src/components/NewPost/NewPost.js
@@ -1,18 +1,45 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
+import { useOpenModalSharedState } from '../../context/OpenModalContext';
 
 function NewPost(props) {
+  const [enteredText, setEnteredText] = useState('test 1');
+  const [enteredAuthor, setEnteredAuthor] = useState('test 2');
+  const { setSharedOpenModalValue } = useOpenModalSharedState()
+
+  function changeBodyHandler(event) {
+    setEnteredText(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const newPostData = {
+      body: enteredText,
+      author: enteredAuthor
+    }
+
+    setEnteredText('')
+    setEnteredAuthor('')
+
+    setSharedOpenModalValue(false)
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p className={classes.inputContainer}>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} value={enteredText} />
       </p>
       <p className={classes.inputContainer}>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" onChange={props.onAuthorChange} required />
+        <input type="text" id="name" onChange={changeAuthorHandler} required value={enteredAuthor} />
       </p>
       <p className={classes.actions}>
-        <button type="button">Add Post</button>
+        <button type="submit">Add Post</button>
         <button type="button" onClick={props.onCancel}>Cancel</button>
       </p>
     </form>
