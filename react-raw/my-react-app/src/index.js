97c4a7886dd7bd449dee3267faa3015a70600f00
guiziis <guigamoda@hotmail.com>
2025-04-14 11:33:30 -0300

    add routes

--- react-raw/my-react-app/src/index.js
+++ react-raw/my-react-app/src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import NewPost from './components/NewPost/NewPost';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'create-post',
+    element: <NewPost />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
