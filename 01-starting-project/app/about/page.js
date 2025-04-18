7df460004a532025d0934422c112c57887297325
guiziis <guigamoda@hotmail.com>
2025-04-18 12:18:01 -0300

    add first component

--- 01-starting-project/app/about/page.js
+++ 01-starting-project/app/about/page.js
@@ -1,6 +1,9 @@
+import { HeaderComponent } from "../components/header";
+
 export default function AboutPage() {
   return (
     <main>
+      <HeaderComponent/>
       <h1>About Page</h1>
       <p>This is the about page of our Next.js application.</p>
     </main>
