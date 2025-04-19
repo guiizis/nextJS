ff16034047e86ae5fba9ff48909cf1ce5ecd5221
guiziis <guigamoda@hotmail.com>
2025-04-19 11:38:57 -0300

    adding mainHeader

---
+++ 05-onwards-foodies-starting-project/app/components/mainHeader.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import logoImg from "../../assets/logo.png";
+
+export default function MainHeader() {
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImg.src} alt="food img" />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <li> <Link href="/meals">Meals</Link></li>
+          <li> <Link href="/community">Community</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
