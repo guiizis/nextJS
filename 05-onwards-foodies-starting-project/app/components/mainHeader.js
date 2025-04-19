cde08e17414c1c54066795a017546d48d391ea7d
guiziis <guigamoda@hotmail.com>
2025-04-19 11:46:11 -0300

    adding css to mainHeader

--- 05-onwards-foodies-starting-project/app/components/mainHeader.js
+++ 05-onwards-foodies-starting-project/app/components/mainHeader.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import logoImg from "../../assets/logo.png";
+import classes from './mainHeader.module.css';
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logoImg.src} alt="food img" />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li> <Link href="/meals">Meals</Link></li>
           <li> <Link href="/community">Community</Link></li>
