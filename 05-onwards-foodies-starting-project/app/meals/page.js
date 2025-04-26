3d96399e3644c058ec7a7847bcb89fa257a389ba
guiziis <guigamoda@hotmail.com>
2025-04-25 21:33:14 -0300

    add mealGrid

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,5 +1,22 @@
+import MealsGrid from '../components/meals/mealsGrid'
+import classes from './page.module.css'
+import Link from 'next/link'
+
 export default function MealsPage() {
   return (
-    <h1>Main meal page</h1>
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favorite recipe and cook it yourself. It's easy and fun</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your favorite recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
