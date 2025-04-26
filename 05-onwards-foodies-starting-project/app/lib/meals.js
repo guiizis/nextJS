383365f6d4f001e3206ab361eb0ca6501f2262d4
guiziis <guigamoda@hotmail.com>
2025-04-26 16:38:44 -0300

    add not-found

--- 05-onwards-foodies-starting-project/app/lib/meals.js
+++ 05-onwards-foodies-starting-project/app/lib/meals.js
@@ -3,6 +3,6 @@ import sql from 'better-sqlite3';
 const db = sql('meals.db')
 
 export function getMeals() {
-  throw new Error('Database not initialized. Please run initdb.js first.');
+  // throw new Error('Database not initialized. Please run initdb.js first.');
   return db.prepare('SELECT * FROM meals').all();
 }
