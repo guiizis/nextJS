5f4086ffb9117fcfabc694914c297883ab8b884a
guiziis <guigamoda@hotmail.com>
2025-04-26 21:57:28 -0300

    add dynamic meal details by click

--- 05-onwards-foodies-starting-project/app/lib/meals.js
+++ 05-onwards-foodies-starting-project/app/lib/meals.js
@@ -6,3 +6,7 @@ export function getMeals() {
   // throw new Error('Database not initialized. Please run initdb.js first.');
   return db.prepare('SELECT * FROM meals').all();
 }
+
+export function getMealBySlug(slug) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
--- 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -1,5 +1,31 @@
-export default function DynamicMealPage({ params }) {
+import Image from 'next/image'
+import classes from './page.module.css'
+import { getMealBySlug } from '@/app/lib/meals'
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMealBySlug(params.mealSlug)
+
   return (
-    <h1>Meal page for {params.mealSlug}</h1>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerContent}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions.replace(/(?:\r\n|\r|\n)/g, '<br />')
+        }}></p>
+      </main>
+    </>
   )
 }
